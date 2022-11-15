94fdfbd26d469bd46048adc571ec372512f58fa3
DavGuett <soulsec@protonmail.com>
2022-11-15 19:32:31 -0300

    Entidade inicial criada

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace ExercicioHeranca_Polimorfismo.Entities;
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public string priceTag()
+    {
+        return Price.ToString();
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the number of products: ");
+int productAmount = int.Parse(Console.ReadLine());
+for (int i = 1; i <= productAmount; i++)
+{
+    
+}
